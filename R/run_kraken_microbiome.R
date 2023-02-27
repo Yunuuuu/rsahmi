4db9deb8a1c5f8feb427e851dc997b9d0dc7472d
yun <yunyunpp96@outlook.com>
2023-02-28 01:33:26 +0800

    replace run_kraken_microbiome with run_krmikm

--- R/run_kraken_microbiome.R
+++ R/run_kraken_microbiome.R
@@ -1,11 +1,21 @@
-#' Run kraken2 and Extract Microbiome reads 
-#' 
-#' A helper function to run both `run_kraken2` and `extract_microbiome`
+#' Run kraken2 and Extract Microbiome reads
+#'
+#' A helper function to run `run_kraken2`, `extract_microbiome` and `run_sckmer`
+#' sequentially.
 #' @param kraken2_cmd The path of kraken2 command.
 #' @inheritParams extract_microbiome
 #' @inheritParams run_kraken2
-#' @export 
-run_kraken_microbiome <- function(fq1, fq2 = NULL, sample = NULL, out_dir = getwd(), ncbi_blast_path = NULL, kraken2_db = NULL, cores = availableCores(), kraken2_cmd = NULL, kraken2_args = character(), python_cmd = NULL, microbiome_pattern = "(?i)Bacteria|Fungi|Viruses", ntaxid = 8000L, ..., sys_args = list()) {
+#' @inheritParams run_sckmer
+#' @export
+run_krmikm <- function(fq1, fq2 = NULL, sample = NULL, out_dir = getwd(),
+                       ncbi_blast_path = NULL, kraken2_db = NULL,
+                       cores = availableCores(), kraken2_cmd = NULL,
+                       kraken2_args = character(), python_cmd = NULL,
+                       microbiome_pattern = "(?i)Bacteria|Fungi|Viruses", ntaxid = 8000L, ranks = c("G", "S"),
+                       cb_len = 16L, umi_len = 10L,
+                       host = 9606L, nsample = 1000L,
+                       kmer_len = 35L, min_frac = 0.5,
+                       ..., sys_args = list()) {
     sample <- sample %||% sub("_0*[12]?\\.(fastq|fq)(\\.gz)?$", "",
         basename(fq1),
         perl = TRUE
@@ -44,9 +54,48 @@ run_kraken_microbiome <- function(fq1, fq2 = NULL, sample = NULL, out_dir = getw
             ext = "kraken.report.mpa.txt"
         ),
         out_dir = out_dir,
-        sample = sample, ...,
+        sample = sample,
         microbiome_pattern = microbiome_pattern,
-        ntaxid = ntaxid,
+        ntaxid = ntaxid, ...,
         sys_args = sys_args
     )
+    fa1 <- sub("fq$", "fa", fq1, perl = TRUE)
+    if (!is.null(fq2)) {
+        fa2 <- sub("fq$", "fa", fq2, perl = TRUE)
+    } else {
+        fa2 <- fq2
+    }
+    run_sckmer(
+        fa1 = fa1, fa2 = fa2,
+        kraken_report = file_path(out_dir, sample,
+            ext = "kraken.report.txt"
+        ),
+        mpa_report = file_path(out_dir, sample,
+            ext = "kraken.report.mpa.txt"
+        ),
+        microbiome_output = file_path(out_dir, sample,
+            ext = "microbiome.output.txt"
+        ),
+        sample = sample, out_dir = out_dir,
+        ranks = ranks, cb_len = cb_len, umi_len = umi_len,
+        host = host, nsample = nsample,
+        kmer_len = kmer_len, min_frac = min_frac,
+        cores = cores
+    )
+    invisible(list(
+        fa1 = fa1, fa2 = fa2,
+        kraken_report = file_path(out_dir, sample,
+            ext = "kraken.report.txt"
+        ),
+        mpa_report = file_path(out_dir, sample,
+            ext = "kraken.report.mpa.txt"
+        ),
+        kraken_out = file_path(out_dir, sample,
+            ext = "kraken.output.txt"
+        ),
+        microbiome_output = file_path(out_dir, sample,
+            ext = "microbiome.output.txt"
+        ),
+        kmer = file_path(out_dir, sample, ext = "sckmer.txt")
+    ))
 }
