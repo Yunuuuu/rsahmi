62e55a3887ac490411fd38e4cbdfd8943788f463
yun <yunyunpp96@outlook.com>
2024-03-30 11:47:36 +0800

    use $slice method to extract element

--- R/utils-polars.R
+++ R/utils-polars.R
@@ -1,5 +1,8 @@
+#' @param .x A series object.
+#' @param .fn Must call `$collect_in_background()` method.
 #' @keywords internal
 #' @importFrom polars pl
+#' @noRd
 polars_lapply <- function(.x, .fn, ..., .progress, .threads = 2L) {
     # .threads <- min(as.integer(.threads), pl$thread_pool_size())
     poos_size <- max(.threads, 1L)
@@ -11,15 +14,16 @@ polars_lapply <- function(.x, .fn, ..., .progress, .threads = 2L) {
     # pools: store the index of result
     # NA means this pool can be used
     pools <- rep_len(NA_integer_, poos_size)
-    i <- pool <- 1L
-    while (i <= n || !all(is.na(pools))) {
+    pool <- 1L
+    i <- 0L
+    while (i < n || !all(is.na(pools))) {
         handle_index <- .subset(pools, pool)
         # if i > n, we skip add task
-        if (i <= n && is.na(handle_index)) {
+        if (i < n && is.na(handle_index)) {
             # this pool can be used, we add task into this pool
             # `.fn()` must return with `$collect_in_background()` method
             # For Series, cannot subset with `[[`
-            results[[i]] <- .fn(.x[i], ...)
+            results[[i]] <- .fn(.x$slice(i, 1L), ...)
             pools[pool] <- i
             i <- i + 1L
         }
@@ -36,7 +40,7 @@ polars_lapply <- function(.x, .fn, ..., .progress, .threads = 2L) {
 
                 # this pool has been released, so we directly
                 # step into next cycle and re-use this pool
-                if (i <= n) next
+                if (i < n) next
             }
         }
 
