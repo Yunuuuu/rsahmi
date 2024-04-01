3669e1c947db8d069afd6aa5dfe3dfe1cf018c29
yun <yunyunpp96@outlook.com>
2024-04-01 23:12:21 +0800

    use `polars` to implement slsd

--- R/slsd.R
+++ R/slsd.R
@@ -1,38 +1,25 @@
 #' Sample level signal denoising
-#' 
+#'
 #' In the low-microbiome biomass setting, real microbes also exhibit a
 #' proportional number of total k-mers, number of unique k-mers, as well as
 #' number of total assigned sequencing reads across samples; i.e. the following
 #' three Spearman correlations are significant when tested using sample-level
 #' data provided in Kraken reports: `cor(#k-mers, #unique k-mers)`,
-#' `cor(#k-mers, #reads)` and `cor(#reads, #unique k-mers)`. 
+#' `cor(#k-mers, #reads)` and `cor(#reads, #unique k-mers)`.
 #'
 #' @param kraken_reports The paths to kraken2uniq report for all samples, often
 #'   in the form of __sample.kraken.report.txt__. It's easy to use [locate_path]
-#'   to create the path. 
+#'   to create the path.
 #' @param samples An atomic character specifying sample names.
 #' @inheritParams read_kr
-#' @inheritParams run_blsd
 #' @return A [data.table][data.table::data.table] of correlation coefficient and
 #'   pvalue for `cor(#k-mers, #unique k-mers)` (r1 and p1), `cor(#k-mers,
 #'   #reads)` (r2 and p2) and `cor(#reads, #unique k-mers)` (r3 and p3).
-#' @export 
-run_slsd <- function(kraken_reports, samples = NULL, method = "spearman", ..., min_number = 3L, min_reads = 2L, min_uniq = 2L) {
-    if (is.null(samples)) {
-        samples <- rep_len(list(NULL), length(kraken_reports))
-    } else if (length(samples) != length(kraken_reports)) {
-        cli::cli_abort("{.arg kraken_reports} and {.arg samples} must have the same length.")
-    }
-    kr_data <- .mapply(
-        function(kraken_report, sample, ...) {
-            read_kr(kraken_report, sample, ...)
-        },
-        list(kraken_report = kraken_reports, sample = samples),
-        list(min_reads = min_reads, min_uniq = min_uniq)
-    )
-    kr_data <- data.table::rbindlist(kr_data, use.names = TRUE)
-
-    kr_data[, .SD[.N > min_number], by = "taxid"][rank %in% c("G", "S")][,
+#' @export
+sahmi_slsd <- function(kreports, method = "spearman", ...,
+                       min_reads = 2L, min_uniq = 2L, min_number = 3L) {
+    kreports <- pl$concat(kreports, how = "vertical")
+    kreports[, .SD[.N > min_number], by = "taxid"][rank %in% c("G", "S")][,
         {
             min_vs_uniq <- stats::cor.test(
                 min, uniq,
