5513d96c9b8003c94cc61715cb8d447785b51895
yun <yunyunpp96@outlook.com>
2024-04-06 12:11:15 +0800

    fix error: argument "MoreArgs" is missing, with no default

--- R/taxa_counts.R
+++ R/taxa_counts.R
@@ -22,7 +22,7 @@ taxa_counts <- function(umi_list, samples = NULL) {
         samples <- samples %||% names(umi_list) %||% seq_along(umi_list)
         umi_list <- .mapply(function(umi, sample) {
             umi$with_columns(sample = pl$lit(sample))
-        }, list(umi = umi_list, sample = samples))
+        }, list(umi = umi_list, sample = samples), NULL)
     } else if (polars::is_polars_df(umi_list)) {
         if (!is.null(samples)) {
             if (length(samples) != 1L) {
