1c9769d390333ae59fb8f83188a9a70caab848cc
Yunuuuu <yunyunpp96@outlook.com>
2025-02-26 04:56:10 +0800

    use imports instead

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,9 @@
 `%||%` <- function(x, y) if (is.null(x)) y else x
 
-pkg_nm <- function() {
-    utils::packageName(topenv(environment()))
+# mimic polars str methods ---------------------------
+# https://rpolars.github.io/man/ExprStr_contains_any.html
+str_contains_any <- function(string, patterns, ...) {
+    str_detect(string = string, pattern = paste0(patterns, collapse = "|"))
 }
 
 internal_file <- function(...) {
@@ -15,39 +17,6 @@ dir_create <- function(path, ...) {
         !dir.create(path = path, showWarnings = FALSE, ...)) {
         cli::cli_abort("Cannot create directory {.path {path}}")
     }
-}
-
-# purrr-like function -------------------------------
-map2 <- function(.x, .y, .f, ..., USE.NAMES = FALSE) {
-    mapply(.f, .x, .y, MoreArgs = list(...), # styler: off
-        SIMPLIFY = FALSE, USE.NAMES = USE.NAMES
-    )
-}
-
-imap <- function(.x, .f, ..., USE.NAMES = FALSE) {
-    map2(.x, names(.x) %||% seq_along(.x), .f, ..., USE.NAMES = USE.NAMES)
-}
-
-transpose <- function(.l) {
-    if (!length(.l)) return(.l) # styler: off
-
-    inner_names <- names(.l[[1]])
-    if (is.null(inner_names)) {
-        fields <- seq_along(.l[[1]])
-    } else {
-        fields <- inner_names
-        names(fields) <- fields
-        .l <- lapply(.l, function(x) {
-            if (is.null(names(x))) names(x) <- inner_names # styler: off
-            x
-        })
-    }
-
-    # This way missing fields are subsetted as `NULL` instead of causing
-    # an error
-    .l <- lapply(.l, as.list)
-
-    lapply(fields, function(i) lapply(.l, .subset2, i))
 }
 
 # mimic polars list methods --------------------------
